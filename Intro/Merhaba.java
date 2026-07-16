dc6495d8baae7432cce3be804c6ba876e40d5792
türkay ürkmez <turkay.urkmez@outlook.com>
2026-07-16 10:26:11 +0300

    Intro to JDK and JVM

---
+++ Intro/Merhaba.java
@@ -0,0 +1,11 @@
+public class Merhaba{
+   public static void main(String[] args){
+     System.out.println("Merhaba Java Dunyasi");
+     System.out.println("Bu mesaji, JVM calistiriyor");     
+   }
+
+  //Java SE 
+  //Java EE
+  //Java ME 
+  //Android 
+}
